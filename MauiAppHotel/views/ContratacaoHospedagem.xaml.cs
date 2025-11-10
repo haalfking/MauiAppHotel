c1d6a8d2f8484efeeef7f79df142c2976b65161e
Daniel Batista <carvalho.daniel.15@gmail.com>
2025-11-10 00:08:06 -0300

    criação pagina sobre

--- MauiAppHotel/views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/views/ContratacaoHospedagem.xaml.cs
@@ -5,5 +5,10 @@ public partial class ContratacaoHospedagem : ContentPage
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+    }
+    private async void OnSobreClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Sobre());
+    }
+
 }
--- MauiAppHotel/views/Sobre.xaml.cs
+++ MauiAppHotel/views/Sobre.xaml.cs
@@ -2,8 +2,13 @@ namespace MauiAppHotel.views;
 
 public partial class Sobre : ContentPage
 {
-	public Sobre()
-	{
-		InitializeComponent();
-	}
+    public Sobre()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnVoltarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
